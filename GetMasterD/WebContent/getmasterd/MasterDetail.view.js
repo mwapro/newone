c861479afebc5450bbb13398d7b6ba937561b2c5
marku_000 <marku_000@AlPadrone>
2017-10-03 22:20:25 +0200

    First try out to add UI5 generated files

---
+++ GetMasterD/WebContent/getmasterd/MasterDetail.view.js
@@ -0,0 +1,24 @@
+sap.ui.jsview("getmasterd.MasterDetail", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf getmasterd.MasterDetail
+	*/ 
+	getControllerName : function() {
+		return "getmasterd.MasterDetail";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf getmasterd.MasterDetail
+	*/ 
+	createContent : function(oController) {
+ 		return new sap.m.Page({
+			title: "Title",
+			content: [
+			
+			]
+		});
+	}
+
+});
